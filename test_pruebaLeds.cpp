308d2dad5cf94cf1c13cf9eed0ca5a59e428fb06
Beneharo NC <alu0100534719@ull.edu.es>
2019-05-21 19:24:30 +0100

    Primer dia

--- test_pruebaLeds.cpp
+++ test_pruebaLeds.cpp
@@ -1,21 +1,24 @@
 //Prueba de leds
 
-#include "BBB_GPIO_pin.hpp"
+#include "BBB_GPIO_pin.hpp" //Declaración de la clase
 
 #include <iostream>
 #include <chrono>
 #include <thread>
 
 int main() {
-	BBB_GPIO_pin p1("P9_41");
 	
-	p1.setDirection("out");
+	BBB_GPIO_pin p1("P9_41"); //Creamos un objeto 'p1' de la clase 'BBB_GPIO_pin'
 	
-	p1.setValue(1);
+	p1.setDirection("out"); //Lo configuramos como salida
 	
-	std::this_thread::sleep_for(std::chrono::seconds(3));
+	p1.setValue(1); //Con valor '1' , es decir, con tensión (3,3V)
 	
-	p1.setValue(0);
+	std::this_thread::sleep_for(std::chrono::seconds(1)); //Parpadea durante 1 segundo
+	
+	p1.setValue(0); //Después del tiempo transcurrido, poner a 0, es decir, apagar
+	
+	
   
 
 }
