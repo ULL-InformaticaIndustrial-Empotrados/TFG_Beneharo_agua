64c274ca7f48a9631a297f38415c3ff2639b07c5
Beneharo NC <alu0100534719@ull.edu.es>
2019-05-22 15:17:30 +0100

    2 leds parpadeando

--- test_pruebaLeds.cpp
+++ test_pruebaLeds.cpp
@@ -1,6 +1,6 @@
-//Prueba de leds
+//Prueba de parpadeo de 2 leds
 
-#include "BBB_GPIO_pin.hpp" //Declaración de la clase
+#include "BBB_GPIO_pin.hpp" //Incluye la clase
 
 #include <iostream>
 #include <chrono>
@@ -9,18 +9,20 @@
 int main() {
 	
 	BBB_GPIO_pin p1("P9_41"); //Creamos un objeto 'p1' de la clase 'BBB_GPIO_pin'
+	BBB_GPIO_pin p2("P9_25");
 	
-	p1.setDirection("out"); //Lo configuramos como salida
-	
-	p1.setValue(1); //Con valor '1' , es decir, con tensión (3,3V)
-	
-	std::this_thread::sleep_for(std::chrono::seconds(1)); //Parpadea durante 1 segundo
-	
-	p1.setValue(0); //Después del tiempo transcurrido, poner a 0, es decir, apagar
-	
-	
-  
-
+	for (int i = 0; i < 10; i++) {
+		p1.setDirection("out"); //Lo configuramos como salida
+		p2.setDirection("out");
+		p1.setValue(1); //Con valor '1' , es decir, con tensión (3,3V)
+		p2.setValue(1);
+		std::this_thread::sleep_for(std::chrono::seconds(1)); //Parpadea durante 1 segundo
+		
+		p1.setValue(0); //Después del tiempo transcurrido, poner a 0, es decir, apagar
+		p2.setValue(0);
+		std::this_thread::sleep_for(std::chrono::seconds(1)); //Parpadea durante 1 segundo
+	}
 }
+
 
 
